a8da7709a0406a84e40465a2a4e5fb2e947c0777
Xuan4781 <Xuan4781@users.noreply.github.com>
2026-03-19 21:05:07 -0400

    bubble prefab and add player shooting

---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 10f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.y = 1;
+
+        Vector3 direction = (mousePos - transform.position).normalized;
+
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+        bullet.GetComponent<Rigidbody>().linearVelocity = direction * bulletSpeed;
+    }
+}
